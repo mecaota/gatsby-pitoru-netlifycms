5436acaec06579d4567b58d2883e43a558a58f67
mecaota <b1014167@fun.ac.jp>
2018-11-28 18:51:09 +0900

    メタ情報のテンプレートを変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'pitoruの多趣味日記++',
+    description: 'ここは、pitoruが趣味の何かをいろいろする為のページです。（アクセス保証は）ないです。',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
