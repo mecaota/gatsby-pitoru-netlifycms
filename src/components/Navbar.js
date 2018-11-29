6d390fcba891b05c34ae04c5a917fd9a3ee192e9
mecaota <b1014167@fun.ac.jp>
2018-11-29 21:35:25 +0900

    navbarのコンポーネントの説明追加・修正

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,20 @@ const Navbar = () => (
     <div className="container">
       <div className="tabs is-boxed is-medium">
         <ul>
-          <li className="is-active"><Link to="/" className="navbar-item" title="Logo">
-            <img src={logo} alt="Home" style={{ height: '24px' }} />
+          <li className="is-active"><Link to="/" className="navbar-item" title="Home">
+            <img src={logo} alt="🏠" style={{ height: '2rem' }} />
             &nbsp;&nbsp;Home
           </Link></li>
-          <li><Link className="navbar-item" to="/about">
+          <li><Link className="navbar-item" to="/about" title="About">
             About
           </Link></li>
-          <li><Link className="navbar-item" to="/products">
+          <li><Link className="navbar-item" to="/products" title="Products">
             Products
           </Link></li>
-          <li><Link className="navbar-item" to="/contact">
+          <li><Link className="navbar-item" to="/contact" title="Contact">
             Contact
           </Link></li>
-          <li><Link className="navbar-item" to="/contact/examples">
+          <li><Link className="navbar-item" to="/contact/examples" title="Form Example">
             Form Examples
           </Link></li>
         </ul>
