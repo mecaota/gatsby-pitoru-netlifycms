ab8b2de1080cdc34fc0ecb0a3ebbafeb9f01def3
mecaota <b1014167@fun.ac.jp>
2018-12-03 19:43:32 +0900

    GDPR対応の為の同意表示を実装

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from "gatsby"
-
+import CookieConsent from "react-cookie-consent";
 import Navbar from '../components/Navbar'
 import './all.sass'
 
@@ -36,6 +36,12 @@ const TemplateWrapper = ({ slug, children }) => (
         </Helmet>
         <Navbar />
         <div>{children}</div>
+        <div className="message">
+          <CookieConsent buttonText="OK" containerClasses="message-header" disableStyles={false} style={{ background:"rgba(27, 94, 32, 0.9)"}} buttonStyle={{background:"#ffffff"}}>
+            <p>当ウェブサイトではアクセス解析の為, Cookie並びにGoogle Analyticsによる情報収集を行っています。</p>
+            <p>This site use Cookies and Google Analytics.</p>
+          </CookieConsent>
+        </div>
       </div>
     )}
   />
