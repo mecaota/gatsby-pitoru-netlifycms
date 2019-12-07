de86738ae0b3ab65196b1347328812470922aaba
mecaota <b1014167@fun.ac.jp>
2019-12-07 22:13:32 +0900

    Revert "最新版gatsbyに追従"

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
-import CookieConsent from 'react-cookie-consent';
+import { StaticQuery, graphql } from "gatsby";
+import CookieConsent from "react-cookie-consent";
 import Navbar from '../components/Navbar';
 import logo from '../img/logo.png';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ slug, children, menu }) => (
         <Navbar menu={menu} />
         <div>{children}</div>
         <div className="message">
-          <CookieConsent buttonText="OK" containerClasses="message-header" disableStyles={false} style={{ background:'rgba(27, 94, 32, 0.9)'}} buttonStyle={{background:'#ffffff'}}>
+          <CookieConsent buttonText="OK" containerClasses="message-header" disableStyles={false} style={{ background:"rgba(27, 94, 32, 0.9)"}} buttonStyle={{background:"#ffffff"}}>
             <p>当ウェブサイトではアクセス解析の為, Cookie並びにGoogle Analyticsによる情報収集を行っています。</p>
             <p>This site use Cookies and Google Analytics.</p>
           </CookieConsent>
@@ -53,6 +53,6 @@ const TemplateWrapper = ({ slug, children, menu }) => (
       </div>
     )}
   />
-);
+)
 
-export default TemplateWrapper;
+export default TemplateWrapper
