f50453f03dcb9c76fe875215d483d1a11caa65e2
mecaota <24618982+mecaota@users.noreply.github.com>
2020-01-25 10:43:41 +0900

    sass形式からscssへmigrate

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import './all.sass';
+import './all.scss';
 library.add(fab, fas, far);
 
 const TemplateWrapper = ({ slug, children, menu }) => (
